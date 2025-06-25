cd15d3e338cf9d0258a5c76e44255c5eafbb58f6
shubhamkumar7979 <shubhamK25968@gmail.com>
2025-06-26 01:30:05 +0530

    first jenkin program

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("This is java program");
         System.out.println("output from java program");
-        System.out.println(new Date());   
+        System.out.println(new Date());
+        System.out.println("first Jenkins program");   
     }
 }
