911ecc0fe2ac1a7345a5a88d08d770dfc7bcdff9
shubhamkumar7979 <shubhamK25968@gmail.com>
2025-06-26 01:46:33 +0530

    new changes

--- shubham.java
+++ shubham.java
@@ -1,12 +1,12 @@
-import java.util.Date;
+// import java.util.Date;
 
-public class Main{
-
+public class shubham {
     public static void main(String[] args) {
         System.out.println("This is java program");
         System.out.println("output from java program");
-        System.out.println(new Date());
+        // System.out.println(new Date());
         System.out.println("first Jenkins program");
-        System.out.println("Hi Vineet");   
+        // System.out.println("Hi Vineet");   
     }
+    
 }
